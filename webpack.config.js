bc1fb03cd01b00cb183dab94e36bad31eadb0177
Vicens Fayos <datracka@gmail.com>
2016-06-21 17:47:42 +0200

    remove webconfig.config.js from gitignore

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require("webpack");
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+
+  entry: {
+    main: "./src/main.ts", //main app entry point
+  },
+  output: {
+    //filename: './app/js/bundle.js'
+    path: __dirname,
+    filename: "./app/js/bundle.js"
+  },
+  resolve: {
+    extensions: ['' , '.js' , '.ts']
+  },
+  module: {
+    loaders: [
+      { test: /\.ts?$/, loader: 'ts-loader' , exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      proxy: 'http://localhost:3100/app/', //BS act as a proxy for webpack-de-server
+      //server: { baseDir: ['./app'] }
+    })
+  ]
+}
