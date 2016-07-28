44872d02b36b17d1ca65ac67483429d0aee4c598
Vicens Fayos <datracka@gmail.com>
2016-07-28 19:50:48 +0200

    add about

---
+++ app/src/about/about.component.ts
@@ -0,0 +1,11 @@
+import {BaseComponent} from '../frameworks/core/index';
+
+@BaseComponent({
+  moduleId: module.id,
+  selector: 'sd-about',
+  templateUrl: 'about.component.html',
+  styleUrls: ['about.component.css']
+})
+export class AboutComponent  {
+  
+}
