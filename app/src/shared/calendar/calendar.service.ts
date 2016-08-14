96bbad65c2b45ed2d0e1f2d7210825652fc76282
Vicens Fayos <datracka@gmail.com>
2016-08-14 12:43:59 +0200

    apply again changes in calender.service and header

--- app/src/shared/calendar/calendar.service.ts
+++ app/src/shared/calendar/calendar.service.ts
@@ -1,20 +1,26 @@
 import {Injectable} from '@angular/core';
-import {Http, Headers, Response} from '@angular/http';
+import {Http, Headers, Response, RequestOptions} from '@angular/http';
 import {contentHeaders} from '../common/headers';
 import {Observable} from "rxjs/Rx";
+import {AuthHttp} from 'angular2-jwt';
 
 @Injectable()
 export class CalendarService {
 
-    private actionUrl:string;
-    private headers:Headers;
+    private actionUrl: string;
+    private headers: Headers;
 
-    constructor(private _http:Http) {
+    private options = new RequestOptions({
+        body: '',
+        headers: contentHeaders,
+    });
+
+    constructor(private _http: Http, private authHttp: AuthHttp) {
         this.actionUrl = process.env.BASE_URL + process.env.API_PATH;
         this.headers = contentHeaders;
     }
 
-    public getAll():Observable<Response> {
-        return this._http.get(this.actionUrl + "calendar/year/2016", {headers: this.headers});
+    public getAll(): Observable<Response> {
+        return this.authHttp.get(this.actionUrl + "calendar/year/2016", this.options);
     }
 }
--- app/src/shared/common/headers.ts
+++ app/src/shared/common/headers.ts
@@ -3,4 +3,3 @@ import { Headers } from '@angular/http';
 export const contentHeaders = new Headers();
 contentHeaders.append('Accept', 'application/json');
 contentHeaders.append('Content-Type', 'application/json');
-contentHeaders.append('Authorization', 'Basic Y2FybG9zOmFzZGY='); //Hardcoded
