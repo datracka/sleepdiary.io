0c1b7511c42745c56ec05ce9bd889d0f71436928
Vicens Fayos <datracka@gmail.com>
2016-08-15 15:45:29 +0200

    shared: update to RC5, remove temporally JWT

--- app/src/shared/calendar/calendar.service.ts
+++ app/src/shared/calendar/calendar.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {Http, Headers, Response, RequestOptions} from '@angular/http';
 import {contentHeaders} from '../common/headers';
 import {Observable} from "rxjs/Rx";
-import {AuthHttp} from 'angular2-jwt';
 
 @Injectable()
 export class CalendarService {
@@ -14,11 +13,11 @@ export class CalendarService {
         headers: contentHeaders,
     });
 
-    constructor(private _http: Http, private authHttp: AuthHttp) {
+    constructor(private _http: Http) {
         this.actionUrl = process.env.BASE_URL + process.env.API_PATH;
     }
 
     public getAll(): Observable<Response> {
-        return this.authHttp.get(this.actionUrl + "calendar/year/2016", this.options);
+        return this._http.get(this.actionUrl + "calendar/year/2016", this.options);
     }
 }
--- app/src/shared/common/auth-guard.ts
+++ app/src/shared/common/auth-guard.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
-import { tokenNotExpired } from 'angular2-jwt';
-
 
 @Injectable()
 export class AuthGuard implements CanActivate {
