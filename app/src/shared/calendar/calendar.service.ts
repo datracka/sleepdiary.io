09b23add71bd8249db2f0f6616149f1cf8667bc6
Vicens Fayos <datracka@gmail.com>
2016-08-16 18:36:21 +0200

    shared: add proper handling for JWT

--- app/src/shared/calendar/calendar.service.ts
+++ app/src/shared/calendar/calendar.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
-import {Http, Headers, Response, RequestOptions} from '@angular/http';
+import {Http, Response, RequestOptions} from '@angular/http';
 import {contentHeaders} from '../common/headers';
 import {Observable} from "rxjs/Rx";
+import {AuthHttp} from 'angular2-jwt';
 
 @Injectable()
 export class CalendarService {
@@ -13,11 +14,11 @@ export class CalendarService {
         headers: contentHeaders,
     });
 
-    constructor(private _http: Http) {
+    constructor(private _http: Http, private _authHttp: AuthHttp) {
         this.actionUrl = process.env.BASE_URL + process.env.API_PATH;
     }
 
     public getAll(): Observable<Response> {
-        return this._http.get(this.actionUrl + "calendar/year/2016", this.options);
+        return this._authHttp.get(this.actionUrl + "calendar/year/2016", this.options);
     }
 }
