cc4b11208e058492b5ecbbab028130bbb092fe23
Vicens Fayos <datracka@gmail.com>
2016-08-18 16:23:14 +0200

    shared: remove userId from Entry

--- app/src/shared/common/Entry.ts
+++ app/src/shared/common/Entry.ts
@@ -1,13 +1,11 @@
 export class Entry {
 
-    public userId: number;
     public uuid: string;
     public date: any;
     public sleepingQuality: string;
     public tirednessFeeling: string;
 
-    constructor(userId: number, uuid: string, date: string, sleepingQuality: string, tirednessFeeling: string) {
-        this.userId = userId;
+    constructor(uuid: string, date: string, sleepingQuality: string, tirednessFeeling: string) {
         this.uuid = uuid;
         this.date = date;
         this.sleepingQuality = sleepingQuality;
