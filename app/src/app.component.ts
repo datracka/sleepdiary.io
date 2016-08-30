31b393bd0d2652a181363d9b93f267de8b1a8ee0
Vicens Fayos <datracka@gmail.com>
2016-08-30 17:31:10 +0200

    first implementation login styles WIP

--- app/src/app.component.ts
+++ app/src/app.component.ts
@@ -1,11 +1,14 @@
 ///<reference path="../../node_modules/@angular/core/src/metadata/lifecycle_hooks.d.ts"/>
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
+const styles = require('./app.component.css');
 const template = require('./app.html');
 
 @Component({
     selector: 'my-app',
     template: template,
+    styles: [styles],
+    encapsulation: ViewEncapsulation.Native
 })
 export class AppComponent {
 }
