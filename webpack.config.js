e0fd0244c8d81c8b1af02ecd525860f25b5331c3
Vicens Fayos <datracka@gmail.com>
2016-12-28 13:32:22 +0100

    update webpack 2 WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,8 @@
-var webpack = require('webpack');
-var path = require('path');
+'use strict';
+let path = require('path');
+let dotEnvPlugin = require('webpack-dotenv-plugin');
 
-var DotenvPlugin = require('webpack-dotenv-plugin');
-var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-var _ref  = require('awesome-typescript-loader');
-
-/** postcss plugins */
-var precss       = require('precss');
-var autoprefixer = require('autoprefixer');
-
-/**
- * Webpack Constants
- */
-const ENV = process.env.NODE_ENV = process.env.ENV = process.env.npm_lifecycle_event;
-console.log("ENV " + ENV);
-// Webpack Config
-
-var webpackConfig = {
+module.exports = {
 
   entry: {
     'polyfills': './src/polyfills.browser.ts',
@@ -25,16 +11,14 @@ var webpackConfig = {
   },
 
   output: {
-    path: './dist'
+    path: './dist',
+    filename: '[name].bundle.js'
   },
 
-  plugins: [],
-
   module: {
-    loaders: [
-      // .ts files for TypeScript
-      { test: /\.ts$/, loaders: ['awesome-typescript-loader', 'angular2-template-loader'] },
-      { test: /\.css$/, loaders: ['to-string-loader', 'css-loader', 'postcss-loader'] },
+    rules: [
+      { test: /\.ts$/, loaders: ['awesome-typescript-loader', 'angular2-template-loader']},
+      { test: /\.css$/, loaders: ['to-string-loader', 'css-loader', 'postcss-loader']},
       { test: /\.html$/, loader: 'raw-loader' },
       {
         test: /\.scss$/,
@@ -47,55 +31,18 @@ var webpackConfig = {
       }
     ]
   },
-  postcss: function () {
-    return [precss, autoprefixer];
-  }
-};
-
-/** plugins **/
-
-webpackConfig.plugins.push(new _ref.CheckerPlugin());
-webpackConfig.plugins.push(new DotenvPlugin({ sample: './.env.default', path: './.env.dev' }))
-if (ENV !== 'test') { //when test don't want add this plugin
-  webpackConfig.plugins.push(new BrowserSyncPlugin({ host: 'localhost', port: 3000, proxy: 'http://localhost:3100'}));
-}
-
-// Our Webpack Defaults
-var defaultConfig = {
-  devtool: 'cheap-module-source-map',
-  cache: true,
-  debug: true,
-  output: {
-    filename: '[name].bundle.js',
-    sourceMapFilename: '[name].map',
-    chunkFilename: '[id].chunk.js'
-  },
 
   resolve: {
-    root: [ path.join(__dirname, 'src') ],
-    extensions: ['', '.ts', '.js']
+    modules: [
+      path.join(__dirname, 'src'),
+      'node_modules'
+    ],
+    extensions: ['.ts', '.js']
   },
 
-  devServer: {
-    historyApiFallback: true,
-    watchOptions: { aggregateTimeout: 300, poll: 1000 },
-    proxy: {
-      '/api/1': {
-        target: 'http://localhost:8000',
-        secure: false,
-      }
-    }
-  },
+  plugins: [
+      new dotEnvPlugin({ sample: './.env.default', path: './.env.dev' })
+  ],
 
-  node: {
-    global: 1,
-    crypto: 'empty',
-    module: 0,
-    Buffer: 0,
-    clearImmediate: 0,
-    setImmediate: 0
-  }
-};
-
-var webpackMerge = require('webpack-merge');
-module.exports = webpackMerge(defaultConfig, webpackConfig);
+  devtool: false,
+}
