eeed6011bfc3cb32961ddc393aa9339417a6221f
Vicens Fayos <datracka@gmail.com>
2017-01-02 14:40:59 +0100

    add all figures with defalt values

--- src/app/statistics/sample/sample.component.ts
+++ src/app/statistics/sample/sample.component.ts
@@ -9,21 +9,41 @@ const template = require('./sample.html');
 })
 export class SampleComponent {
 
+    //bar
+
+    public barChartOptions:any = {
+        scaleShowVerticalLines: false,
+        responsive: true
+    };
+    public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    public barChartType:string = 'bar';
+    public barChartLegend:boolean = true;
+
+    public barChartData:any[] = [
+        {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+        {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    ];
+
+
+    //line
+
+    public lineChartData:Array<any> = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+    ];
+    public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public lineChartType:string = 'line';
+
+    public pieChartType:string = 'pie';
+
     // Pie
-    public pieChartLabels: string[] = ['Good', 'Bad', 'Regular'];
-    public pieChartData: number[] = [300, 500, 100];
-    public pieChartType: string = 'pie';
-    public form = {
-        metricSample: 'sleeping-quality'
-    }
 
-    // events
-    public chartClicked(e: any): void {
-        console.log(e);
-    }
+    public pieChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+    public pieChartData:number[] = [300, 500, 100];
 
-    public chartHovered(e: any): void {
-        console.log(e);
+    public randomizeType():void {
+        this.lineChartType = this.lineChartType === 'line' ? 'bar' : 'line';
+        this.pieChartType = this.pieChartType === 'doughnut' ? 'pie' : 'doughnut';
     }
 
 }
