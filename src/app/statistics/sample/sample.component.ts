d68fb030970a9cceb1197901e4b82e9649fb5f65
Vicens Fayos <datracka@gmail.com>
2017-01-02 18:36:32 +0100

    add statistics fake

--- src/app/statistics/sample/sample.component.ts
+++ src/app/statistics/sample/sample.component.ts
@@ -14,7 +14,7 @@ export class SampleComponent {
 
     public lineChartData: Array<any> = [
         {data: [65, 59, 80, 81, 56, 55, 40], label: 'Sleeping Quality'},
-        {data: [28, 48, 40, 19, 86, 27, 90], label: 'Tiredness Feelings'}
+        {data: [28, 48, 40, 19, 86, 27, 90], label: 'Tiredness Feeling'}
     ];
     public lineChartLabels: Array<any> = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     public lineChartType: string = 'line';
@@ -25,7 +25,12 @@ export class SampleComponent {
 
     public pieChartType: string = 'pie';
     public pieChartLabels: string[] = ['Good', 'Regular', 'Bad'];
-    public pieChartData: number[] = [300, 500, 100];
+    public pieChartLabelsDreams: string[] = ['Good Dreams', 'Bad Dreams', 'No idea'];
+    public pieChartLabelsTrigers: string[] = ['Smoking', 'Sport', 'Stress', 'Eating heavy', 'Alcohol', 'Worries', 'Room conditions', 'Happiness', 'Fear', 'Partner'];
+    public pieChartData1: number[] = [300, 500, 100];
+    public pieChartData2: number[] = [500, 100, 300];
+    public pieChartData3: number[] = [200, 600, 200];
+    public pieChartData4: number[] = [100, 100, 80, 80, 20, 100, 200, 200, 50, 50, 20, 100];
 
     //bar
 
