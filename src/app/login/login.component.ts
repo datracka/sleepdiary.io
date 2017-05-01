ffeca9b2f804f0acbba325344eec653bbebd8d02
Vicens Fayos <datracka@gmail.com>
2017-05-01 19:47:59 +0200

    implemented login & server validation final version

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,9 +6,10 @@ import {LoginModel} from "./LoginModel";
 
 const template = require('./login.html');
 
+// TODO: move constans to external file
+const ERROR_USER_NOT_FOUND = "error_user_not_found";
 const ERROR_FIELD_VALIDATION = 'error_field_validation';
 const ERROR_NOT_ACTIVE = 'error_not_active';
-const ERROR_USER_NOT_FOUND = 'error_user_not_found';
 const IS_REQUIRED = 'required';
 const IS_NOT_EMAIL = 'email';
 const LENGTH_MIN_6 = 'min_length_6'
@@ -34,9 +35,13 @@ export class Login {
 
   public loginModel: LoginModel;
   public loginForm: FormGroup;
+  public userNotExists: boolean;
+  public userIsNotActive: boolean
 
   constructor(public router: Router, public loginService: LoginService, private fb: FormBuilder) {
     this.loginModel = new LoginModel('', '');
+    this.userNotExists = false;
+    this.userIsNotActive = false;
     this.createForm();
   }
 
@@ -56,7 +61,6 @@ export class Login {
       let body: any = JSON.stringify({email, password});
 
       this.loginService.login(body).subscribe(
-
         response => {
 
           localStorage.setItem('id_token', response.json().token_key);
@@ -71,23 +75,35 @@ export class Login {
 
             if (e.fields.email === IS_REQUIRED) {
               this.loginForm.get('email').setErrors({required: true});
-              this.loginForm.get('email').markAsTouched()
+              this.loginForm.get('email').markAsTouched();
             }
 
             if (e.fields.email === IS_NOT_EMAIL) {
               this.loginForm.get('email').setErrors({pattern: {}});
-              this.loginForm.get('email').markAsTouched()
+              this.loginForm.get('email').markAsTouched();
             }
 
             if (e.fields.password === IS_REQUIRED) {
               this.loginForm.get('password').setErrors({required: true});
-              this.loginForm.get('password').markAsTouched()
+              this.loginForm.get('password').markAsTouched();
             }
 
             if (e.fields.password === LENGTH_MIN_6) {
               this.loginForm.get('password').setErrors({minlength: {}});
-              this.loginForm.get('password').markAsTouched()
+              this.loginForm.get('password').markAsTouched();
             }
+          }
+
+          if (e.error === ERROR_USER_NOT_FOUND) {
+            this.userNotExists = true;
+            this.loginForm.get('email').markAsTouched();
+            this.loginForm.get('password').markAsTouched();
+          }
+
+          if (e.error === ERROR_NOT_ACTIVE) {
+            this.userIsNotActive = true;
+            this.loginForm.get('email').markAsTouched();
+            this.loginForm.get('password').markAsTouched();
           }
         }
       );
